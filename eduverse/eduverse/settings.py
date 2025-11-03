346b6b43aaae163a0dd690fde398f4c3a409105a
Noor-Tanvir-Hossain <tanvirrobin11567@gmail.com>
2025-11-03 10:25:51 +0600

    create eduapp and profile model

--- eduverse/eduverse/settings.py
+++ eduverse/eduverse/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'eduapp'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )  
+}
+
 
 ROOT_URLCONF = 'eduverse.urls'
 
